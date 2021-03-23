c385e1f4c3e9eca53139cb7a4c0ad64e3cefb3c6
TomGriffiths88 <tom@tompatrick.dev>
2021-03-23 15:03:47 +0000

    updated time

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let text = document.querySelector('.intro');
 let time = document.querySelector('.time');
 let container = document.querySelector('.container');
 const button = document.querySelector('.button');
-const announcementTime = new Date('2021, 03, 23, 15:00 GMT');
+const announcementTime = new Date('2021, 03, 23, 19:00 GMT');
 
 const blue = '#9ec8ff';
 
